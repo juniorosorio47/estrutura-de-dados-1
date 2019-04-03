39eb3b4d8373dabfca1cc7f63f5de2341aae3207
Junior Osorio <juniorosorio47@gmail.com>
2019-04-03 12:02:51 -0300

    Adicionadas as funções de inserção no início e final da lista dinâmica.

--- Lista Dinâmica/ListaDinEncad.c
+++ Lista Dinâmica/ListaDinEncad.c
@@ -3,17 +3,19 @@
 #include <ListaDinEncad.h>
 
 //Definição do elemento da lista.
-struct elemento{
+struct elemento
+{
     //Campo para dados do elemento.
     struct aluno dados;
     //Campo com o ponteiro para o proximo elemento.
     struct elemento *prox;
 };
- 
+
 typedef struct elemento Elem;
 
 /*--------------------------- FUNÇÃO PRINCIPAL ---------------------------*/
-int main(){
+int main()
+{
     Lista *li;
 
     //Criação da lista
@@ -24,39 +26,49 @@ int main(){
     printf("Tamanho da lista: %d", tamanho);
 
     //Lista cheia
-    if(lista_cheia(Lista* li)){
+    if (lista_cheia(Lista * li))
+    {
         printf("Esto non eqciste!!!");
-    }else{
+    }
+    else
+    {
         printf("Uma lista dinâmica não fica cheia, pois não tem limite definido.");
     }
 
     //Lista vazia
-    if(lista_vazia(li)){
+    if (lista_vazia(li))
+    {
         printf("A lista está vazia.");
-    }else{
+    }
+    else
+    {
         int tam = tamanho_lista(li);
         printf("A lista não está vazia. Seu tamanho eh: %d", tam);
     }
-
 
     //Libera a lista
     libera_lista(li);
 }
 
 /*--------------------------- IMPLEMENTAÇÃO DAS FUNÇÕES ---------------------------*/
+/*---------- FUNÇÕES BASICAS ----------*/
 //Criação da lista
-Lista* cria_lista(){
-    Lista* li = (Lista*) malloc (sizeof (Lista));
-    if( li != NULL)
+Lista *cria_lista()
+{
+    Lista *li = (Lista *)malloc(sizeof(Lista));
+    if (li != NULL)
         *li = NULL;
     return li;
 }
 
 //Liberação da lista
-void libera_lista(Lista* li){
-    if(li != NULL){
-        Elem* no;
-        while((*li) != NULL){
+void libera_lista(Lista *li)
+{
+    if (li != NULL)
+    {
+        Elem *no;
+        while ((*li) != NULL)
+        {
             no = *li;
             *li = (*li)->prox;
             free(no);
@@ -66,12 +78,15 @@ void libera_lista(Lista* li){
 }
 
 //Tamanho da lista
-int tamanho_lista(Lista* li){
+int tamanho_lista(Lista *li)
+{
     //Verifica se a lista existe
-    if(li == NULL) return 0;
+    if (li == NULL)
+        return 0;
     int cont = 0;
-    Elem* no = *li;
-    while(no != NULL){
+    Elem *no = *li;
+    while (no != NULL)
+    {
         cont++;
         no = no->prox;
     }
@@ -80,15 +95,70 @@ int tamanho_lista(Lista* li){
 
 //Lista cheia
 //Uma lista dinâmica não tem limite, ou seja, nunca fica cheia. Só quando acabar a memória do computador
-int lista_cheia(Lista* li){
+int lista_cheia(Lista *li)
+{
     return 0;
 }
 
 //Lista vazia
-int lista_vazia(Lista* li){
-    if(li == NULL)
+int lista_vazia(Lista *li)
+{
+    if (li == NULL)
         return 1;
-    if(*li == NULL)
+    if (*li == NULL)
         return 1;
     return 0;
+}
+
+/*---------- FUNÇÕES DE INSERÇÃO ----------*/
+int insere_lista_inicio(Lista *li, struct aluno al)
+{
+    //Verifica se a lista existe
+    if (li == NULL)
+        return 0;
+    //Cria um novo elemento com a struct Elem e aloca o tamanho do elemento da memória
+    Elem *no = (Elem *)malloc(sizeof(Elem));
+    //Verifica se a alocação da memória funcionou.
+    if (no == NULL)
+        return 0;
+    //Atribui o elemento a ser adicionado como primeiro elemento
+    no->dados = al;
+    //Coloca como proximo a própria lista.
+    no->prox = (*li);
+    //Substitui a lista li com a lista criada.
+    *li = no;
+    //Retorna verdadeiro.
+    return 1;
+}
+
+int insere_lista_final(Lista *li, struct aluno al)
+{
+    //Verifica se a lista existe
+    if (li == NULL)
+        return 0;
+    //Cria um novo elemento com a struct Elem e aloca o tamanho do elemento da memória
+    Elem *no = (Elem *)malloc(sizeof(Elem));
+    //Verifica se a alocação da memória funcionou.
+    if (no == NULL)
+        return 0;
+    //Atribui o valor do novo elemento ao "no".
+    no->dados = al;
+    //Atribui o prox como NULL (Final da lista)
+    no->prox = NULL;
+    //Se a lista estiver adiciona o "no" na lista e será o ultimo elemento.
+    if ((*li) == NULL)
+    {
+        *li = no;
+    }
+    //Se não percorrerá a lista até o elemento antes do NULL para adicionar o "no".
+    else
+    {
+        Elem *aux = *li;
+        while (aux->prox != NULL)
+        {
+            aux = aux->prox;
+        }
+        aux->prox = no;
+    }
+    return 1;
 }
--- Lista Dinâmica/ListaDinEncad.h
+++ Lista Dinâmica/ListaDinEncad.h
@@ -1,5 +1,6 @@
 //Definição da struct aluno que será guardado dentro da lista.
-struct aluno{
+struct aluno
+{
     int matricula;
     char nome[30];
     float n1, n2, n3;
@@ -7,22 +8,28 @@ struct aluno{
 
 //Apelido para lista.
 //Um ponteiro para ponteiro, ou seja, é um ponteiro e tem um ponteiro dentro.
-typedef struct elemento* Lista;
+typedef struct elemento *Lista;
 
 /*---------- DEFINIÇÃO DAS FUNÇÕES ----------*/
 /*---------- FUNÇÕES BASICAS ----------*/
 //Criação da lista, inicializa a lista.
-Lista* cria_lista();
+Lista *cria_lista();
 
 //Liberação da lista
-void libera_lista(Lista* li);
+void libera_lista(Lista *li);
 
 //Tamanho da lista
-int tamanho_lista(Lista* li);
+int tamanho_lista(Lista *li);
 
 //Lista cheia
-int lista_cheia(Lista* li);
+int lista_cheia(Lista *li);
 
 //Lista vazia
-int lista_vazia(Lista* li);
+int lista_vazia(Lista *li);
 
+/*---------- FUNÇÕES DE INSERÇÃO ----------*/
+//Inserir no inicio da lista
+int insere_lista_inicio(Lista *li, struct aluno al);
+
+//Inserir no final da lista
+int insere_lista_final(Lista *li, struct aluno al);
